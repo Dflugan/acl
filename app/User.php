c0b8b20050ab40daa2a745165d580976b1f2a9e3
Dflugan <dflaphp@gmail.com>
2018-03-01 23:10:57 -0300

    Implementeação MIGRATEION tabelas de relacionamento

--- app/User.php
+++ app/User.php
@@ -43,6 +43,12 @@ class User extends Authenticatable
         return $this->id == 1;
     }
 
+    public function papeis()
+    {
+        return $this->belongsToMany(Papel::class)
+    }
+
+
 
 
 }
